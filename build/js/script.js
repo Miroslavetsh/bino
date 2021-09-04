ed495b4a9f07a99e6ce9b97d92ad22a2f9c2fbac
Miroslavetsh <dfgshte@gmail.com>
2021-09-05 02:31:03 +0300

    Fix images paths

--- build/js/script.js
+++ build/js/script.js
@@ -5,7 +5,7 @@ const navPanel = document.getElementById('nav')
 burger.addEventListener('click', () => {
   document.body.classList.toggle('body-active')
   burger.classList.toggle('burger-active')
-  document.getElementById('nav').classList.toggle('nav-active')
+  navPanel.classList.toggle('nav-active')
 })
 
 
